83bea4432cba029a6beaf76733c3a5ce7eec33bd
Syed Hussain Sajjad <hussainofficialx@gmail.com>
2025-11-05 22:14:51 +0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,79 @@
+var ul = document.getElementById("ul");
+var todoInput = document.getElementById("todoInput");
+var btn = document.getElementById("btn");
+var targetId = undefined;
+
+function deleteItem(id) {
+  // console.log("Li Id: ", id);
+  console.log("Li element with the id " + id + " has been removed");
+
+  var targetLi = document.getElementById(id);
+  // console.log(targetLi);
+
+  ul.removeChild(targetLi);
+}
+
+function updateItem() {
+  console.log("Target Id :", targetId);
+
+  var newValue = todoInput.value;
+  var targetLi = document.getElementById(targetId);
+  targetLi.children[0].innerHTML = newValue;
+  todoInput.value = "";
+  btn.removeAttribute("onclick");
+  btn.innerText = "Add Item";
+  btn.setAttribute("onclick", "addItem()");
+  targetId = undefined;
+}
+
+function editItem(id) {
+  // console.log("Li Id: ", id);
+
+  targetId = id;
+
+  var targetLi = document.getElementById(id);
+  var liText = targetLi.children[0].innerHTML;
+  // console.log(liText);
+
+  todoInput.value = liText;
+
+  // Update item functionality...!
+  btn.removeAttribute("onclick");
+  btn.innerText = "Update Item";
+  btn.setAttribute("onclick", "updateItem()");
+}
+
+function addItem() {
+  var inputValue = todoInput.value;
+  var li = document.createElement("li");
+
+  var span = document.createElement("span");
+  var spanText = document.createTextNode(inputValue);
+  span.appendChild(spanText);
+
+  var randomNum = Math.floor(Math.random() * 10000);
+  var id = new Date().getTime() + randomNum;
+  id = id.toString();
+  // console.log("Id: ", id);
+
+  li.setAttribute("id", id);
+
+  var delBtn = document.createElement("button");
+  var delBtnText = document.createTextNode("Delete Item");
+  delBtn.appendChild(delBtnText);
+  delBtn.setAttribute("onclick", "deleteItem(" + id + ")");
+
+  var editBtn = document.createElement("button");
+  var editBtnText = document.createTextNode("Edit Item");
+  editBtn.appendChild(editBtnText);
+  editBtn.setAttribute("onclick", "editItem(" + id + ")");
+
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.appendChild(editBtn);
+
+  ul.appendChild(li);
+  todoInput.value = "";
+}
+
+// console.log(document.children[0]);
